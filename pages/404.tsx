a188090cbfd3b78f788e359d91a92b5bfb45034a
bmangust <ftf.pollux@gmail.com>
2021-06-23 00:44:56 +0300

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import Flex from '../components/Flex/Flex';
+import HomePage from './Home';
+
+export default function Home() {
+  return (
+    <Flex direction="column">
+      <p>
+        <span style={{ fontWeight: 500 }}>404</span>
+        {'  |  Nothing is found'}
+      </p>
+      <Link href="/">
+        <a>Go home</a>
+      </Link>
+    </Flex>
+  );
+}
